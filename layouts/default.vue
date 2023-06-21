42f31f42321e1ea6df82f66faca6e0432233dff8
Sagar-kennect <Sagar-kennect@192.168.1.37>
2023-06-21 13:02:17 +0530

    table

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,30 @@
 <template>
-  <v-app dark>
-    <v-app-bar border flat app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon
-          :icon="!miniVariant ? mdiChevronLeft : mdiChevronRight"
-        ></v-icon>
-      </v-btn>
+  <v-app>
+    <v-app-bar flat app>
       <v-toolbar-title :text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <v-btn icon>
         <v-icon :icon="mdiAccount"></v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" :rail="miniVariant" app>
+    <!-- Navigation -->
+
+    <v-navigation-drawer
+      expand-on-hover
+      class="bg-white"
+      theme="dark"
+      permanent
+      rail
+    >
+      <v-spacer></v-spacer>
       <v-list nav>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :prepend-icon="item.icon"
-          :title="item.title"
-          :to="item.to"
-        >
-        </v-list-item>
+        <v-list-item> <v-icon :icon="mdiAccount"></v-icon></v-list-item>
+        <v-list-item> <v-icon :icon="mdiAccount"></v-icon></v-list-item>
+        <v-list-item> <v-icon :icon="mdiAccount"></v-icon></v-list-item>
       </v-list>
+      <v-spacer></v-spacer>
     </v-navigation-drawer>
+
     <v-main>
       <c-snackbar>
         <v-container fluid>
@@ -32,24 +32,6 @@
         </v-container>
       </c-snackbar>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :location="right ? 'right' : 'left'"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click="right = !right">
-          <v-list-item-action>
-            <v-icon light :icon="mdiRepeat"> </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -63,13 +45,12 @@ import {
   mdiRepeat,
 } from "@mdi/js";
 import CSnackbar from "~~/components/feedback/c-snackbar.vue";
-import { mapActions } from "pinia";
 
 export default {
   name: "DefaultLayout",
   data() {
     return {
-      drawer: false,
+      drawer: true,
       items: [
         {
           icon: mdiApps,
@@ -82,59 +63,13 @@ export default {
           to: "/inspire",
         },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Vuetify.js",
+      title: "Scheme Builder",
       mdiAccount,
       mdiChevronLeft,
       mdiChevronRight,
       mdiRepeat,
-      hashParams: { load: "", opener: "" },
     };
   },
   components: { CSnackbar },
-
-  methods: {
-    ...mapActions(useAuthStore, ["setAuth"]),
-  },
-
-  async mounted() {
-    const resAuthByOpener = await authByOpener();
-    if (resAuthByOpener.ok) {
-      this.setAuth(resAuthByOpener.data, resAuthByOpener.conf);
-      console.log(
-        `%c ${resAuthByOpener.message}`,
-        "color:green; font-weight:bold;font-size:20px"
-      );
-    } else {
-      const resAuthBySessionStorage = await authBySessionStorage();
-      if (resAuthBySessionStorage.ok) {
-        this.setAuth(
-          resAuthBySessionStorage.data,
-          resAuthBySessionStorage.conf
-        );
-        console.log(
-          `%c ${resAuthBySessionStorage.message}`,
-          "color:green; font-weight:bold;font-size:20px"
-        );
-      } else {
-        console.log(
-          `%c AuthBySession: ${resAuthBySessionStorage.message}`,
-          "color:red; font-weight:bold;font-size:20px"
-        );
-        console.log(
-          `%c AuthByOpener: ${resAuthByOpener.message}`,
-          "color:red; font-weight:bold;font-size:20px"
-        );
-        sessionStorage.clear();
-        // this.$router.push({ name: 'login' })
-        // window.location = '' redirect to auth.kennectweb.com
-      }
-    }
-    authUtils.setLogoutReqFromParent(this.logout);
-
-    window.openAdminPage = this.openAdminPage;
-  },
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,101 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower
-            developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank"
-              rel="noopener noreferrer">
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank"
-              rel="noopener noreferrer" title="chat">
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank"
-              rel="noopener noreferrer" title="contribute">
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+  <v-card flat style="background-color: transparent">
+    <v-tabs v-model="tab" hide-slider selected-class="active">
+      <v-tab
+        value="one"
+        density="compact"
+        class="mr-2"
+        style="border: 1px solid gray"
+        >Scheme Setting</v-tab
+      >
+      <v-tab
+        density="compact"
+        value="two"
+        class="mr-2"
+        style="border: 1px solid gray"
+        >Scheme Set</v-tab
+      >
+      <v-tab
+        density="compact"
+        value="three"
+        class="mr-2"
+        style="border: 1px solid gray; border-radius: 50px"
+        >Scheme Set 1
+        <v-icon :icon="mdiCloseCircleOutline" class="ml-3 hover"> </v-icon
+      ></v-tab>
+    </v-tabs>
+  </v-card>
+  <p class="my-4 text-caption">Scheme Set > Plan</p>
+  <v-card class="h-screen">
+    <v-window v-model="tab">
+      <v-window-item value="one"> Scheme Setting </v-window-item>
+      <v-window-item value="two">
+        <v-container fluid>
+          <v-row class="d-flex align-center px-5">
+            <input
+              type="text"
+              placeholder="Search"
+              class="mr-3 px-2 rounded"
+              style="
+                border: 1px solid black;
+                outline: none;
+                height: 35px;
+                width: 400px;
+              "
+            />
+            <v-btn variant="outlined" class="mr-3"> Filter </v-btn>
+            <v-btn variant="outlined" class="mr-3"> Category </v-btn>
+            <div>
+              <v-switch label="Show Archive List" class="mr-3 mt-5"> </v-switch>
+            </div>
+            <v-btn variant="outlined" class="ml-auto bg-black">
+              <v-icon :icon="mdiPlus"> </v-icon>
+              Create Scheme
+            </v-btn>
+          </v-row>
+
+          <div>
+            <v-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Calories</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in tableData" :key="item.name">
+                  <td>{{ item.displayName }}</td>
+                  <td>{{ item.type }}</td>
+                </tr>
+              </tbody>
+            </v-table>
           </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">
-            Nuxt Documentation
-          </a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-container>
+      </v-window-item>
+      <v-window-item value="three"> Three </v-window-item>
+    </v-window>
+  </v-card>
 </template>
+<script>
+import { mdiAccount, mdiCloseCircleOutline, mdiPlus } from "@mdi/js";
+import schemeData from "../data/schems.json";
 
-<script setup lang="ts">
-
+export default {
+  data: () => ({
+    tab: null,
+    mdiAccount,
+    mdiCloseCircleOutline,
+    mdiPlus,
+    tableData: schemeData,
+  }),
+};
 </script>
+
+<style scoped>
+.active {
+  background: white;
+}
+.hover:hover {
+  color: red;
+}
+</style>
